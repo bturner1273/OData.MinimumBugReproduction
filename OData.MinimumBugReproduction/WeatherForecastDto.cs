2e202cfa3b966044900af21d6ddf80dc8b23b12a
Bradley Turner <bturner@bren-tronics.com>
2021-08-26 11:12:52 -0400

    add unversioned

---
+++ OData.MinimumBugReproduction/WeatherForecastDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OData.MinimumBugReproduction
+{
+    public class WeatherForecastDto
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
---
+++ OData.MinimumBugReproduction/WeatherForecastMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OData.MinimumBugReproduction
+{
+    public class WeatherForecastMapperProfile : Profile
+    {
+        public WeatherForecastMapperProfile()
+        {
+            CreateMap<WeatherForecast, WeatherForecastDto>()
+                .ReverseMap();
+        }
+    }
+}
